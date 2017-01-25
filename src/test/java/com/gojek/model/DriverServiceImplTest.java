7aa4c7d30e69ed91737506c7f83df5b4d34a7dce
Pratik Modi <pratik.modi@gmail.com>
2017-01-26 00:47:48 +0530

    Adding Test cases and creating Shadow Jar

---
+++ src/test/java/com/gojek/model/DriverServiceImplTest.java
@@ -0,0 +1,7 @@
+package com.gojek.model;
+
+/**
+ * Created by Pratik on 25-01-2017.
+ */
+public class DriverServiceImplTest {
+}
---
+++ src/test/java/com/gojek/validator/ValidatorTest.java
@@ -0,0 +1,7 @@
+package com.gojek.validator;
+
+/**
+ * Created by Pratik on 25-01-2017.
+ */
+public class ValidatorTest {
+}
